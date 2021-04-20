a69c2ad1afe9e5916af1141714e7884ae59098e1
Thomas Schumann <thomas.schumann@smail.th-koeln.de>
2021-04-20 21:28:47 +0200

    Gedanken zum Datenhaltungkonzept für Key-Value-Store.

--- keyValStore.c
+++ keyValStore.c
@@ -3,3 +3,39 @@
 //
 
 #include "keyValStore.h"
+
+/*
+### Datenhaltungskonzept
+
+Key Value Stores bestehen im Grunde aus einer zweispaltigen Tabelle.
+Eine Spalte enthält den Schlüssel die andere den zugehörigen Wert.
+Sowohl die Schlüssel als auch die Werte können von unterschiedlichen Typen sein.
+Laut Aufgabenstellung sollen sie Alphanummerisch sein.
+
+ Die Speicherung kann im Arbeitsspeicher oder auf dem Festspeicher erfolgen.
+ Beide Speicherungen unterscheiden sich voneinander.
+Im Arbeitsspeicher:
+ - Speicherung als Array, wobei die Position im Array dem Schlüssel entspricht.
+ - Speicherung als verkettete Liste, dabei kann Schlüssel und Wert in einer Struktur gespeichert werden.
+ - Speicherung als Struktur, dabei ist der Name des structs der des Schlüssels und im struct befindet sich der Wert.
+ ... Andere Lösungen oder Kombinationen möglich.
+Im Festspeicher:
+ - Speicherung in einer einzelnen Datei die durch Zeilen und Kommas getrennt wird.
+ - Jeder Schlüssel mit seinem Wert in einer separaten Datei.
+ - Jeder Schlüssel mit seinem Wert in einer separaten Datei in einer Verzeichnisstruktur dessen Aufbau hierarchisch dem des Schlüssels folgt.
+ ... Andere Lösungen und Kombinationen möglich.
+
+### Festlegungen/Vereinbarungen
+
+Festlegungen für den Aufbau des Schlüssels:
+ - Der Schlüssel enthält keine Leerzeichen.
+ - Der Schlüssel enthält keine Sonderzeichen.
+ - Der Schlüssel enthält nur ???Kleinbuchstaben??? und Zahlen.
+ - ???Länge des Schlüssels???
+
+Festlegungen für den Inhalt der Werte:
+ - ???Werte enthalten keine Leerzeichen.???
+ - Werte enthalten keine Sonderzeichen.
+ - Werte bestehen aus Buchstaben und Zahlen.
+ - ???Länge der Werte???
+*/
