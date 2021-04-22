77941c8092b330f00c726af04a92ef90c99fa635
Thomas Schumann <thomas.schumann@smail.th-koeln.de>
2021-04-22 15:04:02 +0200

    Öffentliche Funktionen zu keyValStore.h hinzugefügt. return.txt im Log-Verzeichnis erstellt und erste Returnwerte eingetragen.

--- keyValStore.h
+++ keyValStore.h
@@ -7,12 +7,18 @@
 
 #endif //BS21_KEYVALSTORE_H
 
+// ### const und structs
 //Länge von Key und Value
 const lengthKey = 10;
 const lengthValue = 10;
+
 // struct für Schlüssel und Wert zur Speicherung als verkettete List.
 typedef struct keyValKomb {
     char key[lengthKey];
     char value[lengthValue];
     struct keyValKomb *next;
 };
+// ### Öffentliche Funktionen
+int put(char* key, char* value);
+int get(char* key, char* res);
+int del(char* key);
