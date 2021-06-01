cc0535ea22bb26c6bb43a20f40db101c137f1bcf
Thomas Schumann <thomas.schumann@smail.th-koeln.de>
2021-06-01 14:54:59 +0200

    keyValStore.c verschoben nach legacy. Linked List wird durch Array ersetzt.

---
+++ legacy/keyValStore.h
@@ -0,0 +1,25 @@
+//
+// Created by benja on 20.04.2021.
+//
+
+#ifndef BS21_KEYVALSTORE_H
+#define BS21_KEYVALSTORE_H
+
+//Länge von Key und Value
+#define LENGTH_KEY 100
+#define LENGTH_VALUE 100
+
+// struct für Schlüssel und Wert zur Speicherung als verkettete List.
+
+struct keyValKomb {
+    char key[LENGTH_KEY];
+    char value[LENGTH_VALUE];
+    struct keyValKomb *next;
+};
+
+// ### Öffentliche Funktionen
+int put(char* key, char* value);
+int get(char* key, char* res);
+int del(char* key);
+
+#endif //BS21_KEYVALSTORE_H
